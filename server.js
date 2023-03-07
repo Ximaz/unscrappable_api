e7c45716f959a80fd5dd3cf787dec5d855267a9d
DURAND Malo <malo.durand@epitech.eu>
2023-03-07 11:35:44 +0100

    removing process.env for now.

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-process.env.HOSTNAME = "localhost"
-process.env.PORT = 8080
-
 const express = require("express"),
       app = express(),
-      { HOSTNAME, PORT } = process.env,
       router = {
           api: require("./routes/api.js")
+      },
+      config = {
+          hostname: "localhost",
+          port: 8080
       }
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use("/api/", router.api)
 
-app.listen(PORT, HOSTNAME, () => {
-    console.log(`API Started at http://${HOSTNAME}:${PORT}/`)
+app.listen(config.port, config.hostname, () => {
+    console.log(`API Started at http://${config.hostname}:${config.port}/`)
 })
